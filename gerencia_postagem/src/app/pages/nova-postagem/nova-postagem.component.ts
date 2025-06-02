2df8258b3be4cf3dee7def1e315d693eadb61888
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-02 19:48:15 -0300

    Corrigido --  a estrutura dos componentes reutilizáveis da page 'nova-postagem'.

--- gerencia_postagem/src/app/pages/nova-postagem/nova-postagem.component.ts
+++ gerencia_postagem/src/app/pages/nova-postagem/nova-postagem.component.ts
@@ -3,14 +3,13 @@ import { HeaderComponent } from '../../componentes/header/header.component';
 import { BarraNavegacaoComponent } from '../../componentes/barra-navegacao/barra-navegacao.component';
 import { CriarPostagemComponent } from '../../componentes/criar-postagem/criar-postagem.component';
 import { SubirImagemVideoComponent } from '../../componentes/subir-imagem-video/subir-imagem-video.component';
-import { FormularioPostagemComponent } from '../../componentes/formulario-postagem/formulario-postagem.component';
 import { FeedbackSucessoErroComponent } from '../../componentes/feedback-sucesso-erro/feedback-sucesso-erro.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
 
 
 @Component({
   selector: 'app-nova-postagem',
-  imports: [HeaderComponent, BarraNavegacaoComponent, CriarPostagemComponent, SubirImagemVideoComponent, FormularioPostagemComponent, FeedbackSucessoErroComponent, FooterComponent],
+  imports: [HeaderComponent, BarraNavegacaoComponent, CriarPostagemComponent, SubirImagemVideoComponent, FeedbackSucessoErroComponent, FooterComponent],
   templateUrl: './nova-postagem.component.html',
   styleUrl: './nova-postagem.component.css'
 })
