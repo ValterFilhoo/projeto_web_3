33a6370ede43841c64b00caedb2d15e2359d4d39
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-02 19:41:19 -0300

    Adcionada a estrutura dos componentes reutilizáveis da page 'login'.

--- gerencia_postagem/src/app/pages/login/login.component.ts
+++ gerencia_postagem/src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormularioLoginComponent } from '../../componentes/formulario-login/formulario-login.component';
+import { FeedbackSucessoErroComponent } from '../../componentes/feedback-sucesso-erro/feedback-sucesso-erro.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormularioLoginComponent, FeedbackSucessoErroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
