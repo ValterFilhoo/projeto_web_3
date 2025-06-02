6e2d1da4017d73a0077d23516cb211e8d17e19a9
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-02 19:14:28 -0300

    Realizada a estrutura do componente da página 'Feed', adcionando todos os componentes reutilizáveis da página.

--- gerencia_postagem/src/app/pages/feed/feed.component.ts
+++ gerencia_postagem/src/app/pages/feed/feed.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../componentes/header/header.component';
 import { BarraNavegacaoComponent } from '../../componentes/barra-navegacao/barra-navegacao.component';
 import { FeedPostagensComponent } from '../../componentes/feed-postagens/feed-postagens.component';
+import { BotaoCurtidaComponent } from '../../componentes/botao-curtida/botao-curtida.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
+import { QtdComentariosComponent } from '../../componentes/qtd-comentarios/qtd-comentarios.component';
 
 @Component({
   selector: 'app-feed',
-  imports: [HeaderComponent, BarraNavegacaoComponent, FeedPostagensComponent, FooterComponent],
+  imports: [HeaderComponent, BarraNavegacaoComponent, FeedPostagensComponent, BotaoCurtidaComponent, QtdComentariosComponent, FooterComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
