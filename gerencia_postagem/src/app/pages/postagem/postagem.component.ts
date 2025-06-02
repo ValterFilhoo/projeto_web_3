2632045ad642e1aec374d2af10f6093c90665aa4
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-02 19:28:15 -0300

    Adcionado o componente reutilizavel 'ComentariosComponent' no componente de pages 'Postagem'.

--- gerencia_postagem/src/app/pages/postagem/postagem.component.ts
+++ gerencia_postagem/src/app/pages/postagem/postagem.component.ts
@@ -4,12 +4,13 @@ import { BarraNavegacaoComponent } from '../../componentes/barra-navegacao/barra
 import { DetalhesPostagemComponent } from '../../componentes/detalhes-postagem/detalhes-postagem.component';
 import { BotaoCurtidaComponent } from '../../componentes/botao-curtida/botao-curtida.component';
 import { QtdComentariosComponent } from '../../componentes/qtd-comentarios/qtd-comentarios.component';
+import { ComentariosComponent } from '../../componentes/comentarios/comentarios.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
 
 
 @Component({
   selector: 'app-postagem',
-  imports: [HeaderComponent, BarraNavegacaoComponent, DetalhesPostagemComponent, BotaoCurtidaComponent, QtdComentariosComponent, FooterComponent, ],
+  imports: [HeaderComponent, BarraNavegacaoComponent, DetalhesPostagemComponent, BotaoCurtidaComponent, QtdComentariosComponent, ComentariosComponent, FooterComponent],
   templateUrl: './postagem.component.html',
   styleUrl: './postagem.component.css'
 })
