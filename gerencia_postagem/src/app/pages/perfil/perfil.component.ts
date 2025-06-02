8d603e0c90007d3625878eea563adfd556264be8
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-02 19:39:04 -0300

    Adcionada a estrutura dos componentes reutilizáveis da page 'perfil'.

--- gerencia_postagem/src/app/pages/perfil/perfil.component.ts
+++ gerencia_postagem/src/app/pages/perfil/perfil.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../componentes/header/header.component';
+import { BarraNavegacaoComponent } from '../../componentes/barra-navegacao/barra-navegacao.component';
+import { PerfilUsuarioComponent } from '../../componentes/perfil-usuario/perfil-usuario.component';
+import { PostagensPerfilComponent } from '../../componentes/postagens-perfil/postagens-perfil.component';
+import { FooterComponent } from '../../componentes/footer/footer.component';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [HeaderComponent, BarraNavegacaoComponent, PerfilUsuarioComponent, PostagensPerfilComponent, FooterComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
