75037bf90cc373c660aeaf36f4e2648469b26be8
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2025-06-03 20:09:49 -0300

    Inserido a estrutura do header, da barra de navegação e iniciando a do feed de postagem.

--- gerencia_postagem/src/app/componentes/feed-postagens/feed-postagens.component.ts
+++ gerencia_postagem/src/app/componentes/feed-postagens/feed-postagens.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BotaoCurtidaComponent } from '../botao-curtida/botao-curtida.component';
+import { QtdComentariosComponent } from '../qtd-comentarios/qtd-comentarios.component';
 
 @Component({
   selector: 'app-feed-postagens',
-  imports: [],
+  imports: [BotaoCurtidaComponent, QtdComentariosComponent],
   templateUrl: './feed-postagens.component.html',
   styleUrl: './feed-postagens.component.css'
 })
